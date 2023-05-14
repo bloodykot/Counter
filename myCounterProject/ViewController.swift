acaa830ac47e54e61d8d3634a045494d4de2bbd2
Dmitry Kirdin <kirdined@rambler.ru>
2023-05-14 13:30:51 +0300

    Initial Commit

---
+++ myCounterProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myCounterProject
+//
+//  Created by Dmitry Kirdin on 14.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
